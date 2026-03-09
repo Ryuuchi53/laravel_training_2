96a838ac885ad43607fc35a4ed0584abe24b8798
Muhammad-Adam-53130 <adam@icu.gov.my>
2026-03-09 14:09:44 +0800

    tukar ke bahasa melayu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Anda telah log masuk!') }}
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,12 @@
                 <div class="card bg-white shadow-sm border-0">
                     <div class="card-body text-center">
                         @guest
+                            Sila Log Masuk untuk melihat kandungan di dalam sistem.
                         @else
-                             <h1>Selamat Datang {{ Auth::user()->name }}</h1>
-                             <p>Anda telah Log Masuk ke dalam sistem.</p>
-                             <p>Sila klik butang di bawah untuk ke halaman utama.</p>
-                             <a href="{{ route('home') }}" class="btn btn-primary">{{ __('Kembali') }}</a>
+                            <h1>Selamat Datang {{ Auth::user()->name }}</h1>
+                            <p>Anda telah Log Masuk ke dalam sistem.</p>
+                            <p>Sila klik butang di bawah untuk ke halaman utama.</p>
+                            <a href="{{ route('home') }}" class="btn btn-primary">{{ __('Kembali') }}</a>
                         @endguest
                     </div>
                 </div>
